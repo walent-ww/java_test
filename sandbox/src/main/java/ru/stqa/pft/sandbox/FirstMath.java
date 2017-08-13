8274fe560ee2dab715cad6b687e2b252b9f99020
Unknown <walent.w@gmail.com>
2017-08-13 16:01:58 +0300

    def points

--- sandbox/src/main/java/ru/stqa/pft/sandbox/FirstMath.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/FirstMath.java
@@ -16,9 +16,18 @@ public class FirstMath {
         p2.printPoint();
 
         //distance
+        System.out.println("Расстояние между точками А(" + p1.x + ";"+p1.y + ") и В("
+                + p2.x + ";" + p2.y + ") = "
+                +distance(p1, p2));
+
+        //distance from Point class
         System.out.println(p1.distance(p1, p2));
         System.out.println(p1.distance(new Point(-4,7), new Point(6,8.3)));
         System.out.println(p2.distance(new Point(4,-0.9), new Point(6,8.3)));
 
+    }
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow((p2.y - p1.y), 2) + Math.pow((p2.x - p1.x), 2));
     }
 }
