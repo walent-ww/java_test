697353a71093b987072df3f4b1eb0ea4895c6327
Unknown <walent.w@gmail.com>
2017-09-16 13:58:45 +0300

    csv generator for groups

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generator/GroupDataGenerator.java
@@ -0,0 +1,60 @@
+package ru.stqa.pft.addressbook.generator;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by user on 16.09.17.
+ */
+public class GroupDataGenerator {
+    @Parameter(names = "-c", description = "groups count")
+    public int count;
+    @Parameter(names = "-f", description = "file for write")
+    public String file;
+
+
+    public static void main(String[] args) throws IOException {
+        GroupDataGenerator generator = new GroupDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException pex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+    }
+
+    private void run() throws IOException {
+        List<GroupData> groups = generateGroups(count);
+        save(groups, new File(file));
+    }
+
+    private void save(List<GroupData> groups, File file) throws IOException {
+        Writer writer = new FileWriter(file);
+        for (GroupData g : groups) {
+            writer.write(String.format("%s;%s;%s\n", g.getName(), g.getHeader(), g.getFooter()));
+
+        }
+        writer.close();
+    }
+
+    private List<GroupData> generateGroups(int count) {
+        List<GroupData> groups = new ArrayList<GroupData>();
+        for (int i = 0; i < count; i++) {
+            groups.add(new GroupData().withName(String.format("test%s", i)).
+                    withHeader(String.format("header%s", i)).withFooter(String.format("footer%s", i)));
+        }
+        return groups;
+    }
+
+}
