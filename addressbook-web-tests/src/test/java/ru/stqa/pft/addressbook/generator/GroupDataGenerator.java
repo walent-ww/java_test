05aaa53c3a3ae44f1167065e9338639b9e3a66aa
Unknown <walent.w@gmail.com>
2017-09-16 15:42:13 +0300

    xml writer groups

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generator/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generator/GroupDataGenerator.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.addressbook.generator;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -20,6 +21,8 @@ public class GroupDataGenerator {
     public int count;
     @Parameter(names = "-f", description = "file for write")
     public String file;
+    @Parameter(names = "-df", description = "data format")
+    public String format;
 
 
     public static void main(String[] args) throws IOException {
@@ -36,10 +39,25 @@ public class GroupDataGenerator {
 
     private void run() throws IOException {
         List<GroupData> groups = generateGroups(count);
-        save(groups, new File(file));
+        if (format.equals("scv"))
+            saveAsCsv(groups, new File(file));
+        else if (format.equals("xml")){
+            saveAsXML(groups, new File(file));
+        }
+        else
+            System.out.println("no format, no");
     }
 
-    private void save(List<GroupData> groups, File file) throws IOException {
+    private void saveAsXML(List<GroupData> groups, File file) throws IOException {
+        XStream xstream = new XStream();
+        xstream.processAnnotations(GroupData.class);
+        String xml = xstream.toXML(groups);
+        Writer writer = new FileWriter(file);
+        writer.write(xml);
+        writer.close();
+    }
+
+    private void saveAsCsv(List<GroupData> groups, File file) throws IOException {
         Writer writer = new FileWriter(file);
         for (GroupData g : groups) {
             writer.write(String.format("%s;%s;%s\n", g.getName(), g.getHeader(), g.getFooter()));
