920deeae4137c4aca4ef20086d4c3ddf91df3e59
Unknown <walent.w@gmail.com>
2017-09-17 14:23:00 +0300

    logback

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,11 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+
 public class TestBase {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
@@ -14,9 +19,22 @@ public class TestBase {
         app.init();
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDown() {
         app.stop();
     }
 
+
+    @BeforeMethod
+    public void logTestStart(Method m){
+        logger.info("Run test " + m.getName());
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m){
+        logger.info("Stop test " + m.getName());
+
+    }
+
 }
+
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/group/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/group/GroupCreationTest.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.tests.group;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -69,6 +71,7 @@ public class GroupCreationTest extends TestBase {
 
     @Test(dataProvider = "validGroupXML")
     public void testGroupCreation(GroupData group) {
+
         app.goTo().groupPage();
         Groups before = app.group().all();
         app.group().create(group);
@@ -76,6 +79,7 @@ public class GroupCreationTest extends TestBase {
         Groups after = app.group().all();
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
     }
 
 }
