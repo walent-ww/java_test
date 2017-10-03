eeebe639b76fca5b925c87d0cb58b451ec0a6425
Unknown <walent.w@gmail.com>
2017-10-03 22:57:26 +0300

    mantis config

---
+++ mantis-tests/src/test/resources/config_inc.php
@@ -0,0 +1,17 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = 'M/RMjv5l9NeBQgRfH+4C8jtL6G9sHwT5Q1NRBQWrlTw=';
+$g_default_language = 'english';
+
+$g_signup_use_captcha = OFF;
+
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = "localhost";
+$g_smtp_port = 1025;
